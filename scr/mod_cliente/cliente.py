11d96c35b591e7c70914e6b39888ea378a0d8382
Duda Zanotto <mariaeduardaaz1307@gmail.com>
2023-11-12 17:32:54 -0300

    atualizaçãoo

---
+++ scr/mod_cliente/cliente.py
@@ -0,0 +1,100 @@
+import requests
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from funcoes import Funcoes
+from settings import HEADERS_API, ENDPOINT_CLIENTE
+
+bp_cliente = Blueprint('cliente', __name__, url_prefix="/cliente", template_folder='templates')
+
+@bp_cliente.route('/', methods=['GET', 'POST'])
+def formListaCliente():
+    try:
+        response = requests.get(ENDPOINT_CLIENTE, headers=HEADERS_API)
+        result = response.json()
+
+        if (response.status_code != 200):
+            raise Exception(result[0])
+        
+        return render_template('formListaCliente.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
+
+@bp_cliente.route('/form-cliente/', methods=['POST', 'GET'])
+def formCliente():
+    return render_template('formCliente.html')
+
+@bp_cliente.route('/insert', methods=['GET', 'POST'])
+def insert():
+    try:
+        # dados enviados via FORM
+        id_clinete = request.form['id']
+        nome = request.form['nome']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        comprar_fiado = request.form['comprar_fiado']
+        dia_fiado = request.form['dia_fiado']
+        senha = Funcoes.cifraSenha(request.form['senha'])
+
+        # monta o JSON para envio a API
+        payload = {'id_cliente': id_clinete, 'nome': nome, 'cpf': cpf, 'telefone': telefone, 'comprar_fiado': comprar_fiado, 'dia_fiado': dia_fiado, 'senha': senha }
+        # executa o verbo POST da API e armazena seu retorno
+        response = requests.post(ENDPOINT_CLIENTE, headers=HEADERS_API, json=payload)
+        result = response.json()
+
+        return redirect(url_for('cliente.formListaCliente', msg=result[0]))
+
+    except Exception as e:
+        return redirect(url_for('cliente.formListaCliente', msgErro=e.args[0]))
+    
+@bp_cliente.route("/form-edit-cliente", methods=['GET', 'POST'])
+def formEditCliente():
+    try:
+        # ID enviado via FORM
+        id_clinete = request.form['id']
+        # executa o verbo GET da API buscando somente o cliente selecionado,
+        # obtendo o JSON do retorno
+        response = requests.get(ENDPOINT_CLIENTE + id_clinete, headers=HEADERS_API)
+        result = response.json()
+        if (response.status_code != 200):
+            raise Exception(result[0])
+        # renderiza o form passando os dados retornados
+        return render_template('formCliente.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])    
+
+@bp_cliente.route('/edit', methods=['GET', 'POST'])
+def edit():
+    try:
+        id_clinete = request.form['id']
+        nome = request.form['nome']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        comprar_fiado = request.form['comprar_fiado']
+        dia_fiado = request.form['dia_fiado']
+        senha = Funcoes.cifraSenha(request.form['senha'])
+
+        payload = {'id_clinete': id_clinete, 'nome': nome, 'cpf': cpf, 'telefone': telefone, 'comprar_fiado': comprar_fiado, 'dia_fiado': dia_fiado, 'senha': senha }
+        # executa o verbo PUT da API e armazena seu retorno
+        response = requests.put(ENDPOINT_CLIENTE + id_clinete, headers=HEADERS_API, json=payload)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        return redirect(url_for('cliente.formListaCliente', msg=result[0]))
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
+
+@bp_cliente.route('/delete', methods=['POST', 'GET'])
+def delete():
+    try:
+        # dados enviados via FORM
+        id_produto = request.form['id_clinete']
+        # executa o verbo DELETE da API e armazena seu retorno
+        response = requests.delete(ENDPOINT_CLIENTE + id_produto, headers=HEADERS_API)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        # return redirect(url_for('funcionario.formListaFuncionario', msg=result[0]))
+        return jsonify(erro=False, msg=result[0])
+    except Exception as e:
+    # return render_template('formListaFuncionario.html',
+        return jsonify(erro=True, msgErro=e.args[0])
+
---
+++ scr/mod_produto/produto.py
@@ -0,0 +1,103 @@
+import requests
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from settings import HEADERS_API, ENDPOINT_PRODUTO
+import base64
+
+bp_produto = Blueprint('produto', __name__, url_prefix="/produto", template_folder='templates')
+
+@bp_produto.route('/', methods=['GET', 'POST'])
+def formListaProduto():
+    try:
+
+        response = requests.get(ENDPOINT_PRODUTO, headers=HEADERS_API)
+        result = response.json()
+
+        if (response.status_code != 200):
+            raise Exception(result[0])
+        
+        return render_template('formListaProduto.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaProduto.html', msgErro=e.args[0])
+
+@bp_produto.route('/form-produto/', methods=['POST', 'GET'])
+def formProduto():
+    return render_template('formProduto.html')
+
+@bp_produto.route('/insert', methods=['GET', 'POST'])
+def insert():
+    try:
+        # dados enviados via FORM
+        id_produto = request.form['id']
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        valor_unitario = request.form['valor_unitario']
+
+        foto = "data:" + request.files['foto'].content_type + ";base64," + str(base64.b64encode(request.files['foto'].read()), "utf-8")
+
+        # monta o JSON para envio a API
+        payload = {'id_produto': id_produto, 'nome': nome, 'descricao': descricao, 'foto': foto, 'valor_unitario': valor_unitario }
+        
+        # executa o verbo POST da API e armazena seu retorno
+        response = requests.put(ENDPOINT_PRODUTO + id_produto, headers=HEADERS_API, json=payload)
+        result = response.json()
+
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        
+        return jsonify(erro=False, msg=result[0])
+    except Exception as e:
+       
+        return jsonify(erro=True, msgErro=e.args[0])
+
+@bp_produto.route("/form-edit-produto", methods=['GET', 'POST'])
+def formEditProduto():
+    try:
+        # ID enviado via FORM
+        id_produto = request.form['id']
+        # executa o verbo GET da API buscando somente selecionado,
+        # obtendo o JSON do retorno
+        response = requests.get(ENDPOINT_PRODUTO + id_produto, headers=HEADERS_API)
+        result = response.json()
+        if (response.status_code != 200):
+            raise Exception(result[0])
+        # renderiza o form passando os dados retornados
+        return render_template('formProduto.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaProduto.html', msgErro=e.args[0])    
+
+@bp_produto.route('/edit', methods=['GET', 'POST'])
+def edit():
+    try:
+        # dados enviados via FORM
+        id_produto = request.form['id']
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        valor_unitario = request.form['valor_unitario']
+        foto = "data:" + request.files['foto'].content_type + ";base64," + str(base64.b64encode(request.files['foto'].read()), "utf-8")
+        
+        # monta o JSON para envio a API
+        payload = {'id_produto': id_produto, 'nome': nome, 'descricao': descricao, 'foto': foto, 'valor_unitario': valor_unitario }
+        # executa o verbo PUT da API e armazena seu retorno
+        response = requests.put(ENDPOINT_PRODUTO + id_produto, headers=HEADERS_API, json=payload)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        return redirect(url_for('produto.formListaProduto', msg=result[0]))
+    except Exception as e:
+        return render_template('formListaProduto.html', msgErro=e.args[0])
+    
+@bp_produto.route('/delete', methods=['POST', 'GET'])
+def delete():
+    try:
+        # dados enviados via FORM
+        id_produto = request.form['id_produto']
+        # executa o verbo DELETE da API e armazena seu retorno
+        response = requests.delete(ENDPOINT_PRODUTO + id_produto, headers=HEADERS_API)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        # return redirect(url_for('funcionario.formListaFuncionario', msg=result[0]))
+        return jsonify(erro=False, msg=result[0])
+    except Exception as e:
+    # return render_template('formListaFuncionario.html',
+        return jsonify(erro=True, msgErro=e.args[0])
